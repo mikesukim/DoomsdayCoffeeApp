f782e80f1fa23fa2efc185c57d7054f59e36dd15
Brian Dearing <smartsnake1@gmail.com>
2020-09-28 18:42:29 -0500

    Expo Works
    Expo now works.
    
    You can use your personal device for testing.

--- assets/igutil.js
+++
@@ -1,6 +0,0 @@
-https://api.instagram.com/oauth/authorize
-  ?client_id={instagram-app-id}
-  &redirect_uri={redirect-uri}
-  &scope={scope}
-  &response_type=code
-  &state={state} 
--- ios/Pods/Target Support Files/Pods-DoomsdayCoffeeApp/Pods-DoomsdayCoffeeApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DoomsdayCoffeeApp : NSObject
-@end
-@implementation PodsDummy_Pods_DoomsdayCoffeeApp
-@end
