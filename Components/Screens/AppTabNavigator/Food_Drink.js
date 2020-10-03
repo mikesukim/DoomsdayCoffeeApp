1dfe6cd7a29edeb8bc4e7eec69d0563ffaad6f24
mikesukim <mikesungunkim@gmail.com>
2020-10-03 16:30:57 -0500

    Update Food_Drink.js
    Update barebone functionalities

--- Components/Screens/AppTabNavigator/Food_Drink.js
+++ Components/Screens/AppTabNavigator/Food_Drink.js
@@ -5,67 +5,51 @@ import {
     StyleSheet
 } from "react-native";
 
-import { Icon } from 'native-base'
-
+// Component Declaration using HOOK
 function Food_Drink() {
     
     // Declare Datas
-    const [foodsData, setFoodsData] = useState(0);
-    const [drinksData, setDrinksData] = useState([]);
-  
-    // Similar to componentDidMount and componentDidUpdate:  
+    const [foodsData, setFoodsData] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+
+    // DidcompononentMount
     useEffect(() => { 
         
         //Define Request
-        const getDrinksData= async () => {
-                try {
-                    let response = await fetch(
-                        // 'http://192.168.1.105:5000/drinks'
-                        'http://192.168.1.42/drinks'
-                    );
-                    let json = await response.json();
-                    this.setDrinksData({
-                        drinksData: json
-                    });
-                } catch (error) {
-                  console.error(error);
-                }
-        };
-        const getFoodsData= async () => {
-            try {
-                let response = await fetch(
-                    // 'http://192.168.1.105:5000/drinks'
-                    'http://192.168.1.42/foods'
-                );
-                let json = await response.json();
-                this.setFoodsData({
-                    foodsData: 1
-                });
-            } catch (error) {
-              console.error(error);
-            }
-        };
+        async function getFoodsData() {
+            const res = await fetch("http://192.168.1.42:8080/foods");
+            res
+              .json()
+              .then(res => setFoodsData(res))
+              .then(console.log(foodsData))
+              .then(setIsLoading(false))
+              .catch(err =>console.error(err));
+          }
 
         // Initiate Request
-        // getDrinksData();
         getFoodsData();
 
     });
 
+
+    //Render
+    if(!isLoading){
+        return foodsData.map((food) => {
+            return (
+              <View><Text>{food.Name}</Text></View>
+            )
+          })
+    }
+    else{
         return (
-            <View style={styles.container}>
-                <Text>FOOD_DRINK</Text>
-                <Text>{foodsData}</Text>
-            </View>
-        );
+            <View><Text>loading</Text></View>
+        )
+    }
   }
 
+
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
 });
 
 
